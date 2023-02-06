0c2596a6323b9945917aa8464622821073a282a3
Alexander Timmins <alexanderstimmins@gmail.com>
2023-02-06 16:04:13 +0000

    All reviews visible in review page however styling needs addressing sooner rather than later

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,34 +1,36 @@
-import {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
-import {getReviews} from '../utils/api'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getReviews } from '../utils/api';
 
-function Reviews(reviewId){
-    const [reviews, setReviews] = useState([])
-    const {review} = useParams()
+function Reviews(reviewId) {
+  const [reviews, setReviews] = useState([]);
 
-    useEffect(() => {
-        getReviews(review).then((reviewsFromAPI) => {
-            setReviews(reviewsFromAPI)
-    })
-}, [reviewId])
+  const { review } = useParams();
+
+  useEffect(() => {
+    getReviews(review).then((reviewsFromAPI) => {
+      setReviews(reviewsFromAPI);
+    });
+  }, [reviewId]);
 
 
-    return(
-        <div>
-        <ul className='List'>
-          {reviews.map((review) => (
-            <p className='review' key={review.review_id}>
-              {review.title} 
-              <img
-                className='reviewImg'
-                src={review.review_img_url}
-                alt={review.review_body}
-              ></img>
-            </p>
-          ))}
-        </ul>
+  return (
+    <div className='ReviewList'>
+      {reviews.map((review) => (
+        <div className='Review' key={review.review_id}>
+          <h2 className='reviewTitle'>{review.title} </h2>
+          <img
+            className='reviewImg'
+            src={review.review_img_url}
+            alt={review.review_body}
+          ></img>
+          <h2 className='reviewOwner'>{review.owner} </h2>
+          <h3 className='reviewBody'>{review.review_body}</h3>
+          <h3 className='reviewUpvotes'>Upvotes: {review.votes}</h3>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default Reviews
+export default Reviews;
