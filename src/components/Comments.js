5732b312c3b197f49e153752d369bb2a01e3ff5a
Alexander Timmins <alexanderstimmins@gmail.com>
2023-02-08 08:43:27 +0000

    voting functionaility finished for both reviews and comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getComments } from '../utils/api';
+import { getComments, voteCommentFunc } from '../utils/api';
 
 function Comments() {
   const [comments, setComments] = useState([]);
+  const [errors, setErrors] = useState(false);
+  const [currentComment, setCurrentComment] = useState([]);
+  const [vote, setVote] = useState(false);
 
   const { review_Id } = useParams();
 
@@ -13,16 +16,55 @@ function Comments() {
     });
   }, [review_Id]);
 
+  function upvote(comment) {
+    console.log('vote');
+    if (vote === false) {
+      voteCommentFunc(comment.comment_id, 1)
+        .then(() => {
+          setVote(true);
+          setErrors(false);
+          comment.votes++;
+        })
+        .catch((err) => {
+          comment.votes--;
+          console.log(err);
+          setVote(false);
+          setErrors(true);
+        });
+    } else {
+      voteCommentFunc(comment.comment_id, -1)
+        .then(() => {
+          setVote(false);
+          setErrors(false);
+          comment.votes--;
+        })
+        .catch((err) => {
+          comment.votes++;
+          console.log(err);
+          setVote(true);
+          setErrors(true);
+        });
+    }
+  }
 
   return (
-    <div className="Comments">
-        <h1 className="CommentTitle">Comments</h1>
+    <div className='Comments'>
+      <h1 className='CommentTitle'>Comments</h1>
       {comments.map((comment) => (
-        <ul key={comment.comment_id}>
-            <h2 className="CommentAuthor">{comment.author}</h2>
-          <h3 className="CommentBody">{comment.body}</h3>
-          <h3 className="CommentVotes"><img className="ThumbsUp" src="http://clipart-library.com/new_gallery/4-46046_graphic-techflourish-collections-yellow-thumbs-up-emoji-android.png"></img> {comment.votes}</h3>
-        </ul>
+        <ol key={comment.comment_id}>
+          <h2 className='CommentAuthor'>{comment.author}</h2>
+          <h3 className='CommentBody'>{comment.body}</h3>
+          <h3 className='CommentVotes'>
+            <img
+              onClick={function () {
+                upvote(comment);
+              }}
+              className='ThumbsUp'
+              src='https://png.pngtree.com/png-vector/20210629/ourlarge/pngtree-red-youtube-like-button-png-image_3538748.jpg'
+            ></img>
+            {comment.votes} {errors ? <div>Try again later</div> : <div></div>}
+          </h3>
+        </ol>
       ))}
     </div>
   );
